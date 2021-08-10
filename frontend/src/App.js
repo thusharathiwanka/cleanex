957c8a6c9a7169f56727b051c481360b6910dd6f
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-08-10 23:42:44 +0530

    DELETE: unwanted files removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<h1 className="text-blue-500 text-5xl">App</h1>
+			<h1 className="text-blue-500 text-5xl">APP</h1>
 		</div>
 	);
 }
