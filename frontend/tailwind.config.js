2f113cb15a0ff3d13b4e153317f7b1669dad5d36
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-08-11 01:34:56 +0530

    UPDATE: base font families changed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ["Inter", "sans-serif"],
+			serif: ["Merriweather", "serif"],
+		},
 	},
 	variants: {
 		extend: {},
