68f165ef78c2765823120966149e9efc0f041c54
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-08-14 15:53:36 +0530

    CREATE: register page animations added

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import TShirtImage from "../../src/assets/images/hanging-t-shirt.png";
+import RightBottomBubble from "../assets/images/right-bottom-bubble-reason.png";
 
 const Register = () => {
 	document.title = "CLEANEX - Sign Up";
 
 	return (
-		<div className="text-gray-800 max-w-screen-2.5xl ml-auto max-h-88 pl-4 flex justify-between items-center overflow-hidden">
+		<div className="text-gray-800 max-w-screen-2.5xl ml-auto max-h-88 pl-4 flex justify-between items-center overflow-hidden relative">
 			<div className="font-semibold text-lg">
 				<h1
 					data-aos="fade-up"
@@ -16,7 +17,10 @@ const Register = () => {
 					Create an Account
 				</h1>
 				<form className="">
-					<div className="flex flex-col justify-start pb-5">
+					<div
+						className="flex flex-col justify-start pb-5"
+						data-aos="fade-up-left"
+					>
 						<label htmlFor="name" className="pb-1">
 							Name
 						</label>
@@ -27,7 +31,11 @@ const Register = () => {
 							className="outline-none rounded-full border px-4 py-3 focus:border-light-blue"
 						/>
 					</div>
-					<div className="flex flex-col justify-start pb-5">
+					<div
+						className="flex flex-col justify-start pb-5"
+						data-aos-delay="50"
+						data-aos="fade-up-left"
+					>
 						<label htmlFor="email" className="pb-1">
 							Email Address
 						</label>
@@ -38,7 +46,11 @@ const Register = () => {
 							className="outline-none rounded-full border px-4 py-3 focus:border-light-blue focus:border-2"
 						/>
 					</div>
-					<div className="flex flex-col justify-start pb-5">
+					<div
+						className="flex flex-col justify-start pb-5"
+						data-aos-delay="100"
+						data-aos="fade-up-left"
+					>
 						<label htmlFor="name" className="pb-1">
 							Password
 						</label>
@@ -49,7 +61,11 @@ const Register = () => {
 							className="outline-none rounded-full border px-4 py-3 focus:border-light-blue"
 						/>
 					</div>
-					<div className="flex flex-col justify-start pb-5">
+					<div
+						className="flex flex-col justify-start pb-5"
+						data-aos-delay="150"
+						data-aos="fade-up-left"
+					>
 						<label htmlFor="mobile" className="pb-1">
 							Mobile Number
 						</label>
@@ -60,20 +76,30 @@ const Register = () => {
 							className="outline-none rounded-full border px-4 py-3 focus:border-light-blue"
 						/>
 					</div>
-					<p>
+					<p data-aos-delay="200" data-aos="fade-up-left">
 						Already have an Account ? &nbsp;
 						<Link to="auth/login" className="text-light-blue">
 							Sign In
 						</Link>
 					</p>
-					<button className="bg-light-blue text-white py-4 px-14 rounded-full mt-8 font-semibold">
+					<button
+						className="bg-light-blue text-white py-4 px-14 rounded-full mt-8 font-semibold"
+						data-aos-delay="250"
+						data-aos="fade-up-left"
+					>
 						Sign Up
 					</button>
 				</form>
 			</div>
 			<div className="max-h-88">
-				<img src={TShirtImage} alt="t-shirt" />
+				<img src={TShirtImage} alt="t-shirt" data-aos="fade-left" />
 			</div>
+			<img
+				src={RightBottomBubble}
+				alt="right-bottom-bubble"
+				className="absolute -z-2 top-20 w-60 left-1/3"
+				data-aos="fade-bottom"
+			/>
 		</div>
 	);
 };
