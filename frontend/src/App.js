04aebee7cf93b5fc6a0d52efd93ec61e12c8263c
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-08-18 15:48:17 +0530

    CREATE: login form input controlling added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import AOS from "aos";
+import axios from "axios";
 
 import Routes from "./routes/Routes";
 
@@ -8,6 +9,8 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 function App() {
+	axios.defaults.baseURL = "http://localhost:5000/";
+
 	useEffect(() => {
 		AOS.init({ duration: 800, easing: "ease-in-out-quad" });
 	});
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import TShirtImage from "../../src/assets/images/hanging-t-shirt.png";
 import RightBottomBubble from "../assets/images/right-bottom-bubble-reason.png";
 
 const Login = () => {
 	document.title = "CLEANEX - Sign In";
+
+	const [customer, setCustomer] = useState({ email: "", password: "" });
+
+	const loginCustomer = async (e) => {
+		e.preventDefault();
+
+		const res = await axios.post("customers/login", customer);
+		console.log(res);
+	};
 
 	return (
 		<div className="text-gray-800 max-w-screen-2.5xl ml-auto max-h-88 pl-4 flex justify-between items-center overflow-hidden relative">
@@ -16,7 +26,7 @@ const Login = () => {
 				>
 					Sign into your Account
 				</h1>
-				<form>
+				<form onSubmit={loginCustomer}>
 					<div
 						className="flex flex-col justify-start pb-5"
 						data-aos="fade-up-left"
@@ -29,6 +39,10 @@ const Login = () => {
 							name="email"
 							id="email"
 							className="outline-none rounded-full border px-4 py-3 focus:border-light-blue focus:border-2"
+							value={customer.email}
+							onChange={(e) =>
+								setCustomer({ ...customer, email: e.target.value })
+							}
 						/>
 					</div>
 					<div
@@ -44,6 +58,10 @@ const Login = () => {
 							name="password"
 							id="password"
 							className="outline-none rounded-full border px-4 py-3 focus:border-light-blue"
+							value={customer.password}
+							onChange={(e) =>
+								setCustomer({ ...customer, password: e.target.value })
+							}
 						/>
 					</div>
 					<p data-aos-delay="100" data-aos="fade-up-left">
@@ -56,6 +74,7 @@ const Login = () => {
 						className="bg-light-blue text-white py-4 px-14 rounded-full mt-8 font-semibold"
 						data-aos-delay="150"
 						data-aos="fade-up-left"
+						type="submit"
 					>
 						Sign In
 					</button>
