f6f65b53796d6ff7bdf15a2477bfae666f8b5fd6
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-08-20 01:50:30 +0530

    FIX: server crash issues fixed

--- backend/api/routes/blog.routes.js
+++ backend/api/routes/blog.routes.js
@@ -1,9 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const {getAllBlogs, getBlog ,addBlogs, deleteBlogs, updateBlogs} = require('../controllers/blog.controller')
+const {
+	getAllBlogs,
+	getBlog,
+	addBlogs,
+	deleteBlogs,
+	updateBlogs,
+} = require("../controllers/blog.controller");
 
-router.get("/get",getAllBlogs);
+router.get("/get", getAllBlogs);
 router.get("/get", getBlog);
-router.post("/post",addBlogs);
-router.delete("/delete",deleteBlogs);
-router.patch("/ubdate",updateBlogs);
+router.post("/post", addBlogs);
+router.delete("/delete", deleteBlogs);
+router.patch("/ubdate", updateBlogs);
+
+module.exports = router;
