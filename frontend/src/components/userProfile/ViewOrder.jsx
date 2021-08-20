a2a56409b029a229625c38783d5a8e49df212a8e
OnellaNatalie <onellanatalie824@gmail.com>
2021-08-20 15:49:30 +0530

    USerprofile UI design

---
+++ frontend/src/components/userProfile/ViewOrder.jsx
@@ -0,0 +1,128 @@
+import React from "react";
+
+const ViewOrder = () => {
+  return (
+    <div class="flex flex-col">
+      <div className="flex mx-auto items-center justify-center shadow-lg mt-56  mb-4">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <h2 class="text-5xl font-extrabold pb-10 pt-10 text-center ml-8 ">
+            Order View
+          </h2>
+          <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg m-8">
+              <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-blue-500">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider"
+                    >
+                      Img
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider"
+                    >
+                      Quantity
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider"
+                    >
+                      Laundry Item
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider"
+                    >
+                      Price Per Item
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                  <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0 h-10 w-10">
+                          <img
+                            class="h-10 w-10 rounded-lg"
+                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900 border-2 text-center rounded-lg">
+                        3
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">Cotton T-Shirt</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">8900</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0 h-10 w-10">
+                          <img
+                            class="h-10 w-10 rounded-lg"
+                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900 border-2 text-center rounded-lg">
+                        3
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">Cotton T-Shirt</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">8900</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0 h-10 w-10">
+                          <img
+                            class="h-10 w-10 rounded-lg"
+                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900 border-2 text-center rounded-lg">
+                        3
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">Cotton T-Shirt</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">8900</div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <button class="transition duration-500 ease-in-out py-3 px-5 m-8 ml-96 bg-black hover:bg-blue-500 text-white rounded-3xl transform hover:-translate-y-1 hover:scale-110">
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ViewOrder;
--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -16,62 +16,66 @@ import AdminNewPackage from "../pages/AdminNewPackage";
 import AdminCustomer from "../pages/AdminCustomer";
 import AdminFeedback from "../pages/AdminFeedback";
 import Payment from "../pages/Payment";
+import UserProfile from "../pages/UserProfile";
 
 const Routes = () => {
-	return (
-		<>
-			<Switch>
-				<Route exact path="/">
-					<Navbar />
-					<Home />
-				</Route>
-				<Route exact path="/auth/register">
-					<Navbar />
-					<Register />
-				</Route>
-				<Route exact path="/auth/login">
-					<Navbar />
-					<Login />
-				</Route>
-				<Route exact path="/auth/user/packages">
-					<Navbar />
-					<Packages />
-				</Route>
-				<Route exact path="/auth/admin/dashboard">
-					<AdminDashboard />
-				</Route>
-				<Route exact path="/auth/admin/packages">
-					<AdminPackages />
-				</Route>
-				<Route exact path="/auth/admin/packages/new">
-					<AdminNewPackage />
-				</Route>
-				<Route exact path="/auth/admin/customers">
-					<AdminCustomer />
-				</Route>
-				<Route exact path="/auth/admin/feedbacks">
-					<AdminFeedback />
-				</Route>
-				<Route exact path="/worker">
-					<Navbar />
-					<Worker />
-				</Route>
-				<Route exact path="/createblogs">
-					<Navbar />
-					<CreateBlog />
-				</Route>
-				<Route exact path="/blogs">
-					<Blog />
-				</Route>
-				<Route exact path="/singleblog">
-					<SingleBlog />
-				</Route>
-				<Route exact path="/payment">
-					<Payment/>
-				</Route>
-			</Switch>
-		</>
-	);
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Navbar />
+          <Home />
+        </Route>
+        <Route exact path="/auth/register">
+          <Navbar />
+          <Register />
+        </Route>
+        <Route exact path="/auth/login">
+          <Navbar />
+          <Login />
+        </Route>
+        <Route exact path="/auth/user/packages">
+          <Navbar />
+          <Packages />
+        </Route>
+        <Route exact path="/auth/admin/dashboard">
+          <AdminDashboard />
+        </Route>
+        <Route exact path="/auth/admin/packages">
+          <AdminPackages />
+        </Route>
+        <Route exact path="/auth/admin/packages/new">
+          <AdminNewPackage />
+        </Route>
+        <Route exact path="/auth/admin/customers">
+          <AdminCustomer />
+        </Route>
+        <Route exact path="/auth/admin/feedbacks">
+          <AdminFeedback />
+        </Route>
+        <Route exact path="/worker">
+          <Navbar />
+          <Worker />
+        </Route>
+        <Route exact path="/createblogs">
+          <Navbar />
+          <CreateBlog />
+        </Route>
+        <Route exact path="/blogs">
+          <Blog />
+        </Route>
+        <Route exact path="/singleblog">
+          <SingleBlog />
+        </Route>
+        <Route exact path="/payment">
+          <Payment />
+        </Route>
+        <Route exact path="/userprofile">
+          <UserProfile />
+        </Route>
+      </Switch>
+    </>
+  );
 };
 
 export default Routes;
