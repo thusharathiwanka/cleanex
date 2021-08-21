8e2ba05e0f62ea31864a284ecb345d9d01454396
hasansin <63632399+hasansin@users.noreply.github.com>
2021-08-21 21:50:19 +0530

    cartReducer added location pop up added

--- frontend/src/contexts/CartContext.jsx
+++ frontend/src/contexts/CartContext.jsx
@@ -1,33 +1,37 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 import uuid from "uuid/dist/v1";
+import { cartReduser } from "../reducers/cartReducer";
 
 export const CartContext = createContext();
 
 const CartContextProvider = (props) => {
-	const [packages, setPackages] = useState([
-		{
-			id: "1",
-			package: {
-				_id: "61207cf29e205839985698b9",
-				status: "active",
-				createdAt: "1629331200000",
-				updatedAt: "1629417600000",
-				name: "Shirt on Hanger",
-				description: "Washed, Pressed and Hung",
-				price: "200",
-			},
-			quantity: "2",
-		},
-	]);
-	const addPackage = (Package, Quantity) => {
-		setPackages([...packages, { Package, Quantity, id: uuid() }]);
-	};
-
-	const removePacakge = (id) => {
-		setPackages(packages.filter((packages) => packages.id !== id));
-	};
+	const [packages, dispatch] = useReducer(cartReduser, [], () => {
+		const localData = localStorage.getItem("packages");
+		return localData
+			? JSON.parse(localData)
+			: [
+					{
+						id: "2",
+						pack: [
+							{
+								_id: "61207cf29e205839985698b9",
+								status: "active",
+								createdAt: "1629331200000",
+								updatedAt: "1629417600000",
+								name: "Shirt on Hanger",
+								description: "Washed, Pressed and Hung",
+								price: "200",
+							},
+							{ quantity: "2" },
+						],
+					},
+			  ];
+	});
+	useEffect(() => {
+		localStorage.setItem("packages", JSON.stringify(packages));
+	}, [packages]);
 	return (
-		<CartContext.Provider value={{ packages, removePacakge, addPackage }}>
+		<CartContext.Provider value={{ packages, dispatch }}>
 			{props.children}
 		</CartContext.Provider>
 	);
