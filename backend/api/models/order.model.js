aaaeafd1078b6dfc55a0059be17686c3557f3520
hasansin <63632399+hasansin@users.noreply.github.com>
2021-08-21 14:14:36 +0530

    laundry bag component changed

--- backend/api/models/order.model.js
+++ backend/api/models/order.model.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-    items:[{type: Object}],
-    WashingStatus:{type: String },
-    PickupStatus:{type: String},
-    Total:{type: String},
-    DelevaryStatus:{type: String },
-    Hours:{type: String},
-    StartDate:{type: String},
-    StartTime:{type: String},
-    CompletedDate:{type: String},
-    CompletedTime:{type: String}
-})
+	items: [{ type: Object }],
+	WashingStatus: { type: String },
+	PickupStatus: { type: String },
+	Total: { type: String },
+	DelevaryStatus: { type: String },
+	Hours: { type: String },
+	StartDate: { type: String },
+	StartTime: { type: String },
+	CompletedDate: { type: String },
+	CompletedTime: { type: String },
+});
+const Order = mongoose.model("Orders", orderSchema);
+module.exports = Order;
