3c9d245b7748714c3375ef6d9a75b9524cb4e033
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-08-23 01:01:22 +0530

    CREATE: get single package endpoint created

--- frontend/src/helpers/sidebarLinks.js
+++ frontend/src/helpers/sidebarLinks.js
@@ -1,6 +1,5 @@
 import { MdDashboard, MdStyle, MdFeedback } from "react-icons/md";
 import { FaUsers } from "react-icons/fa";
-import { BsBookHalf } from "react-icons/bs";
 import { RiLogoutBoxFill } from "react-icons/ri";
 import { IoDocumentText } from "react-icons/io5";
 
@@ -13,7 +12,6 @@ const sidebarLinksAdmin = [
 	},
 	{ name: "Customers", path: "/auth/admin/customers", icon: <FaUsers /> },
 	{ name: "Feedbacks", path: "/auth/admin/feedbacks", icon: <MdFeedback /> },
-	{ name: "Blogs", path: "/auth/admin/blogs", icon: <BsBookHalf /> },
 	{ name: "Reports", path: "/auth/admin/reports", icon: <IoDocumentText /> },
 	{ name: "Sign Out", path: "", icon: <RiLogoutBoxFill /> },
 ];
