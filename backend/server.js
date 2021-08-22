d8bbd17c1bcc3c56233ef9dc63eb155368f0cb7f
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-08-22 10:49:42 +0530

    UPDATE: image path added as public

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,17 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
-app.use("/public/images", express.static(__dirname + "/public/uploads/"));
+app.use(
+	"/api/public/uploads",
+	express.static(path.join(__dirname, "/api/public/uploads"))
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
