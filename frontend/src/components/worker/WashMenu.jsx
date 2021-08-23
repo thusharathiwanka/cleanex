ac0a8fe55796128a6d06e6ea2af16f4cb34f2249
jayasith <it19079264@my.sliit.lk>
2021-08-23 21:37:39 +0530

    change menu in Washmenu.jsx

--- frontend/src/components/worker/WashMenu.jsx
+++ frontend/src/components/worker/WashMenu.jsx
@@ -10,7 +10,7 @@ const WashMenu = (props) => {
     return (
         <div >
             <div class=" flex justify-evenly  font-bold text-3xl">
-            <span onClick={()=>{setList("pending")}} className="cursor-pointer ">Pending</span>
+            <span onClick={()=>{setList("pending") }} className="cursor-pointer ">Pending</span>
             <span onClick={()=>{setList("processing")}} className="cursor-pointer ">Processing</span>
             <span onClick={()=>{setList("completed")}} className="cursor-pointer ">Completed</span> 
             </div> 
