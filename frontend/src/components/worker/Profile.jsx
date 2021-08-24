2cacd64a36c3aba0940ca08ce2e39ac8881d4bf7
jayasith <it19079264@my.sliit.lk>
2021-08-25 00:15:32 +0530

    change all "class" to "className"

--- frontend/src/components/worker/Profile.jsx
+++ frontend/src/components/worker/Profile.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 const Profile = (props) => {
     return (
-        <div class="md:flex ml-14 rounded-xl p-5 md:p-10">
+        <div className="md:flex ml-14 rounded-xl p-5 md:p-10">
             <img src={props.Img}  alt='Worker profile' className=" rounded-lg" width='150' height='150'/>
-            <div  class="pt-10 pl-8 text-center md:text-left">
-            <h2 class="text-2xl font-semibold">{props.name}</h2>
-            <h3 class="text-2xl font-normal text-light-blue">{props.role}</h3>
+            <div  className="pt-10 pl-8 text-center md:text-left">
+            <h2 className="text-2xl font-semibold">{props.name}</h2>
+            <h3 className="text-2xl font-normal text-light-blue">{props.role}</h3>
             </div>
             
         </div>
