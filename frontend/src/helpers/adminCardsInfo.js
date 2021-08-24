790f58b14c6a0385420c4c0719dbbf06201fdb84
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-08-25 02:30:43 +0530

    FIX: admin info card order issue fixed

--- frontend/src/helpers/adminCardsInfo.js
+++ frontend/src/helpers/adminCardsInfo.js
@@ -2,11 +2,12 @@ import { MdStyle, MdFeedback } from "react-icons/md";
 import { FaUsers } from "react-icons/fa";
 
 const adminCardInfo = [
-	{ name: "Total Packages", icon: <MdStyle /> },
-	{ name: "Total Customers", icon: <FaUsers /> },
+	{ name: "Total Packages", icon: <MdStyle />, endpoint: "/packages/total" },
+	{ name: "Total Customers", icon: <FaUsers />, endpoint: "/customers/total" },
 	{
 		name: "Total Feedbacks",
 		icon: <MdFeedback />,
+		endpoint: "/feedbacks/total",
 	},
 ];
 
