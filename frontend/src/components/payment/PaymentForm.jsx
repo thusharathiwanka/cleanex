98932ef73335ff0353ad2427a03109d278d0c5bd
jayasith <it19079264@my.sliit.lk>
2021-08-25 22:39:25 +0530

    payment form changed

--- frontend/src/components/payment/PaymentForm.jsx
+++ frontend/src/components/payment/PaymentForm.jsx
@@ -38,7 +38,7 @@ const PaymentForm = (props) => {
                 Card Number
             </label>
             <br/>
-            <input required className="mt-3 text-lg bg-light-gary appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-light-blue"  type="text" placeholder="xxxx-xxxx-xxxx-xxxx"/>
+            <input pattern="[0-9]{16}" required className="mt-3 text-lg bg-light-gary appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-light-blue"  type="text" placeholder="xxxx-xxxx-xxxx-xxxx"/>
             <br/><br/>
             <label  className="text-lg text-gray-500 font-bold   pr-4" for="inline-password">
                 Expiry Date
@@ -48,12 +48,12 @@ const PaymentForm = (props) => {
             </label>
             <br/>
                 <div className="mt-3">
-                    <input required className="bg-light-gary appearance-none text-lg border-2 border-gray-200 rounded w-20 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-light-blue"  type="text" placeholder="MM"/>
-                    <label className="text-lg text-gray-500 font-bold  pl-5   pr-4" for="inline-password">
+                    <input pattern="[0-9]{2}" min="1" max="12" required className="bg-light-gary appearance-none text-lg border-2 border-gray-200 rounded w-20 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-light-blue"  type="text" placeholder="MM"/>
+                    <label  className="text-lg text-gray-500 font-bold  pl-5   pr-4" for="inline-password">
                         /
                     </label>
-                    <input required className="bg-light-gary  text-lg appearance-none border-2 border-gray-200 rounded w-20 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-light-blue"  type="text" placeholder=" YY"/>
-                    <input required className="bg-light-gary  text-lg ml-14 w-60  appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-light-blue"  type="text" placeholder="***"/>
+                    <input pattern="[0-9]{2}" min="21" max="30" required className="bg-light-gary  text-lg appearance-none border-2 border-gray-200 rounded w-20 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-light-blue"  type="text" placeholder=" YY"/>
+                    <input required className="bg-light-gary  text-lg ml-14 w-60  appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-light-blue" pattern="[0-9]{3}" type="text" placeholder="***"/>
                 </div>
             <button className="ml-56 shadow mt-10  bg-light-blue text-white font-semibold py-1 px-8 text-xl  rounded-full "  type="submit">
                 PAY
