1d40c69b80b99d4a50b5bb273915ec0745370676
jayasith <it19079264@my.sliit.lk>
2021-09-19 22:39:42 +0530

    Report generation

--- frontend/src/components/manager/PaymentList.jsx
+++ frontend/src/components/manager/PaymentList.jsx
@@ -6,6 +6,9 @@ import Graph from './Graph';
 import axios from 'axios';
 import { IoArrowBackOutline } from "react-icons/io5";
 import { IoIosClose } from "react-icons/io";
+import { jsPDF } from "jspdf";
+import Logo from "../../assets/images/logo-blue.png";
+import 'jspdf-autotable'
 
 const PaymentList = () => {
 
@@ -13,8 +16,6 @@ const PaymentList = () => {
     const [Total, setTotal] = useState(0)
     const [SearchValue, setSearchValue] = useState("")
     const [NoResults, setNoResults] = useState(false)
-    
-
     
     
     async function fetchPaymentDetails(){
@@ -28,6 +29,33 @@ const PaymentList = () => {
         }catch(err){
             console.log(err);
         }
+    }
+
+    const generateReport = ()=>{
+
+        const report = new jsPDF();
+        const date = new Date();
+
+        report.addImage(Logo, "png", 10, 10, 30, 5);
+        report.setFontSize(20);
+        report.text("Payment Report", 75, 14);
+        report.setFontSize(8);
+        report.text("Name : Chamindu Jayasith", 10, 25);
+        report.text(`Date   : ${date.toDateString()}`, 10, 30);
+        report.text(`Time   : ${date.toLocaleTimeString([],{hour:'2-digit',minute:'2-digit'})}`, 10, 35);
+        report.setFontSize(13);
+        report.text(`Total Amount : LKR${Total}.00`, 135, 45);
+        report.autoTable({
+            margin: { top: 50 },
+            columns:[{ header: 'Name', dataKey: 'name' },
+                    { header: 'Date', dataKey: 'dates' },
+                    { header: 'Amount(LKR)', dataKey: 'amount' }],
+            body:PaymentDetails.map(id=>({
+                name:id.name, dates:id.date, amount:id.amount+'.00'
+            })),    
+        })
+        report.save(`payment-report-${Date()}.pdf`);
+
     }
 
     useEffect(() => fetchPaymentDetails(), [])
@@ -93,13 +121,13 @@ const PaymentList = () => {
                             return(<div key={id.name} className=" bg-white ml-3 mb-5 mt-2 rounded-lg p-5 px-8 relative shadow-xl w-11/12">
                                 <span className="  font-medium">{id.name}</span><br/>
                                 <span className="text-light-blue font-medium">{id.date}</span>
-                                <span className=" absolute right-8 top-7 text-2xl font-semibold">LKR {id.amount}</span>
+                                <span className=" absolute right-8 top-7 text-2xl font-semibold">LKR {id.amount}.00</span>
                             </div>)})}
                         </div>)
                     }
                     </div>
                 </div>
-                <button className="bg-light-blue rounded-full absolute right-24 top-100 text-white  font-semibold text-xl py-3 px-5">Generate Report</button>
+                <button onClick={generateReport} className="bg-light-blue rounded-full absolute right-24 top-100 text-white  font-semibold text-xl py-3 px-5">Generate Report</button>
             </div>
     )
 }
