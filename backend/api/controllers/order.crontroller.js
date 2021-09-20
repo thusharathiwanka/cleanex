8b54f1b58fadeea29ae0ca7d402f77f9ddca9c11
OnellaNatalie <onellanatalie824@gmail.com>
2021-09-20 23:50:45 +0530

    oder
    view order controller, route

--- backend/api/controllers/order.crontroller.js
+++ backend/api/controllers/order.crontroller.js
@@ -1,119 +1,129 @@
 const order = require("../models/order.model");
 
 const updateToProcess = async (req, res) => {
-	try {
-		await order.findByIdAndUpdate(req.params.id, {
-			WashingStatus: "processing",
-			Hours: req.body.hours,
-		});
-		res.status(200).json({ message: "order successfully approved" });
-	} catch (err) {
-		res.status(200);
-		console.log(err.message);
-	}
+  try {
+    await order.findByIdAndUpdate(req.params.id, {
+      WashingStatus: "processing",
+      Hours: req.body.hours,
+    });
+    res.status(200).json({ message: "order successfully approved" });
+  } catch (err) {
+    res.status(200);
+    console.log(err.message);
+  }
 };
 
 const updateToCompleate = async (req, res) => {
-	try {
-		await order.findByIdAndUpdate(req.params.id, {
-			WashingStatus: "completed",
-		});
-		res.status(200).json({ message: "order successfully approved" });
-	} catch (err) {
-		res.status(200);
-		console.log(err.message);
-	}
+  try {
+    await order.findByIdAndUpdate(req.params.id, {
+      WashingStatus: "completed",
+    });
+    res.status(200).json({ message: "order successfully approved" });
+  } catch (err) {
+    res.status(200);
+    console.log(err.message);
+  }
 };
 
 const getPendingOrders = async (req, res) => {
-	try {
-		const pendingOrdes = await order.find({
-			WashingStatus: "pending",
-		});
-		res.status(200).json({ pendingOrdes: pendingOrdes });
-	} catch (err) {
-		res.status(200);
-		console.log(err.message);
-	}
+  try {
+    const pendingOrdes = await order.find({
+      WashingStatus: "pending",
+    });
+    res.status(200).json({ pendingOrdes: pendingOrdes });
+  } catch (err) {
+    res.status(200);
+    console.log(err.message);
+  }
 };
 
 const getProcessingOrders = async (req, res) => {
-	try {
-		const processingOrdes = await order.find({
-			WashingStatus: "processing",
-		});
-		res.status(200).json({ processingOrdes: processingOrdes });
-	} catch (err) {
-		res.status(200);
-		console.log(err.message);
-	}
+  try {
+    const processingOrdes = await order.find({
+      WashingStatus: "processing",
+    });
+    res.status(200).json({ processingOrdes: processingOrdes });
+  } catch (err) {
+    res.status(200);
+    console.log(err.message);
+  }
 };
 
 const getCompletedOrders = async (req, res) => {
-	try {
-		const completedOrdes = await order.find({
-			WashingStatus: "completed",
-		});
-		res.status(200).json({ completedOrdes: completedOrdes });
-	} catch (err) {
-		res.status(200);
-		console.log(err.message);
-	}
+  try {
+    const completedOrdes = await order.find({
+      WashingStatus: "completed",
+    });
+    res.status(200).json({ completedOrdes: completedOrdes });
+  } catch (err) {
+    res.status(200);
+    console.log(err.message);
+  }
 };
 
 const addOrder = async (req, res) => {
-	try {
-		const newOrder = new order(req.body);
-		await newOrder.save();
-		res.status(200);
-	} catch (error) {
-		res.status(400);
-		console.log(error);
-	}
+  try {
+    const newOrder = new order(req.body);
+    await newOrder.save();
+    res.status(200);
+  } catch (error) {
+    res.status(400);
+    console.log(error);
+  }
 };
 
 const getAllOrders = async (req, res) => {
-	try {
-		const orders = await order.find();
-		res.status(200).json(orders);
-	} catch (error) {
-		res.status(404).json({ message: error.message });
-	}
+  try {
+    const orders = await order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+const getUserOrders = async (req, res) => {
+  console.log(req.body);
+  try {
+    const orders = await order.findById(req.body.userId);
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 const getByIdOrder = async (req, res) => {
-	try {
-		const order = await order.findById(req.params.id);
-		res.status(200).json(order);
-	} catch (error) {
-		res.status(404).json({ message: error.message });
-	}
+  try {
+    const order = await order.findById(req.params.id);
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 const updateDeliveryStatus = async (req, res) => {
-	const order = await order.findByIdAndUpdate(req.body.id, {});
+  const order = await order.findByIdAndUpdate(req.body.id, {});
 };
 
 const updateDeliverID = async (req, res) => {
-	try {
-		const deliverer = await order.findByIdAndUpdate(req.params.id, {
-			DelivaryId: req.body.userId,
-		});
-		res.status(202).json(deliverer);
-	} catch (error) {
-		res.status(404).json({ message: error.message });
-	}
+  try {
+    const deliverer = await order.findByIdAndUpdate(req.params.id, {
+      DelivaryId: req.body.userId,
+    });
+    res.status(202).json(deliverer);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 module.exports = {
-	addOrder,
-	getAllOrders,
-	getByIdOrder,
-	updateDeliveryStatus,
-	updateDeliverID,
-	getPendingOrders,
-	getProcessingOrders,
-	getCompletedOrders,
-	updateToProcess,
-	updateToCompleate,
+  addOrder,
+  getAllOrders,
+  getByIdOrder,
+  getUserOrders,
+  updateDeliveryStatus,
+  updateDeliverID,
+  getPendingOrders,
+  getProcessingOrders,
+  getCompletedOrders,
+  updateToProcess,
+  updateToCompleate,
 };
--- backend/api/routes/order.routes.js
+++ backend/api/routes/order.routes.js
@@ -1,31 +1,33 @@
 const router = require("express").Router();
 
 const {
-	getPendingOrders,
-	getProcessingOrders,
-	getCompletedOrders,
-	addOrder,
-	getAllOrders,
-	updateDeliveryStatus,
-	updateDeliverID,
-	getByIdOrder,
-	updateToProcess,
-	updateToCompleate,
+  getPendingOrders,
+  getProcessingOrders,
+  getCompletedOrders,
+  addOrder,
+  getAllOrders,
+  getUserOrders,
+  updateDeliveryStatus,
+  updateDeliverID,
+  getByIdOrder,
+  updateToProcess,
+  updateToCompleate,
 } = require("../controllers/order.crontroller");
 
 const { verifyDelivererAuth } = require("../middleware/delivererAuth");
+const { verifyCustomerAuth } = require("../middleware/customerAuth");
 
 router.post("/addOrder", addOrder);
 router.get("/orders", getAllOrders);
 
 router.put("/updateOrder", updateDeliveryStatus);
 router.put("/deliverer/:id", verifyDelivererAuth, updateDeliverID);
-router.get("/:id", getByIdOrder);
-
+router.get("/userOrders", verifyCustomerAuth, getUserOrders);
 router.get("/getPendingOrders", getPendingOrders);
 router.get("/getProcessingOrders", getProcessingOrders);
 router.get("/getCompletedOrders", getCompletedOrders);
 router.patch("/updateToProcess/:id", updateToProcess);
 router.patch("/updateToCompleate/:id", updateToCompleate);
+router.get("/:id", getByIdOrder);
 
 module.exports = router;
