550c858011e9cb8dbb3ee2fcfe355a39b1ffcac2
OnellaNatalie <onellanatalie824@gmail.com>
2021-10-01 09:38:58 +0530

    add icons for the user profile side bar

--- frontend/src/helpers/usersidebarLink.js
+++ frontend/src/helpers/usersidebarLink.js
@@ -1,10 +1,25 @@
-import { RiLogoutBoxFill } from "react-icons/ri";
+import {
+  RiLogoutBoxFill,
+  RiHome2Fill,
+  RiUser3Fill,
+  RiEdit2Fill,
+  RiFeedbackFill,
+  RiChatHistoryFill,
+} from "react-icons/ri";
 const usersidebar = [
-  { name: "Home", path: "/" },
-  { name: "Profile", path: "/auth/user/profile" },
-  { name: "Edit Profile", path: "/auth/user/userprofileeditdelete" },
-  { name: "Feedback", path: "/auth/user/feedback" },
-  { name: "All order history", path: "/auth/user/allorderhistory" },
+  { name: "Home", path: "/", icon: <RiHome2Fill /> },
+  { name: "Profile", path: "/auth/user/profile", icon: <RiUser3Fill /> },
+  {
+    name: "Edit Profile",
+    path: "/auth/user/userprofileeditdelete",
+    icon: <RiEdit2Fill />,
+  },
+  { name: "Feedback", path: "/auth/user/feedback", icon: <RiFeedbackFill /> },
+  {
+    name: "All order history",
+    path: "/auth/user/allorderhistory",
+    icon: <RiChatHistoryFill />,
+  },
   { name: "Sign Out", path: "", icon: <RiLogoutBoxFill /> },
 ];
 export { usersidebar };
